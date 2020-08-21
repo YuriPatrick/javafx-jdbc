4b074d27c1046d4578941ce9e6ff119ca8161c22
Yuri Furquim <yuripatrick347@gmail.com>
2020-08-21 09:24:36 -0300

    TextField & DatePicker

--- src/gui/util/Constraints.java
+++ src/gui/util/Constraints.java
@@ -5,27 +5,18 @@ import javafx.scene.control.TextField;
 public class Constraints {
 
 	public static void setTextFieldInteger(TextField txt) {
-
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-
 			if (newValue != null && !newValue.matches("\\d*")) {
-
 				txt.setText(oldValue);
-
 			}
-
 		});
 
 	}
 
 	public static void setTextFieldMaxLength(TextField txt, int max) {
-
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-
 			if (newValue != null && newValue.length() > max) {
-
 				txt.setText(oldValue);
-
 			}
 
 		});
@@ -33,15 +24,10 @@ public class Constraints {
 	}
 
 	public static void setTextFieldDouble(TextField txt) {
-
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-
 			if (newValue != null && !newValue.matches("\\d*([\\.]\\d*)?")) {
-
 				txt.setText(oldValue);
-
 			}
-
 		});
 
 	}
